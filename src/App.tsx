fb12c57301ac2710ad58136a7a8275b7cad4efae
dennis-1210 <dennis1210@deltafi.ai>
2021-08-05 16:40:31 -0400

    implement 404 not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
 import React, { lazy } from 'react';
-import { Router, Route, Switch } from 'react-router-dom'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 
 import SuspenseWithChunkError from './components/SuspenseWithChunkError'
 import PageLoader from './components/PageLoader'
-import Header from './components/Header'
+
+import CustomRoute from './CustomRoute'
 
 import GlobalStyle from './style/Global'
-import history from './routerHistory'
 
 const Home = lazy(() => import('./views/Home'))
 const NotFound = lazy(() => import('./views/NotFound'))
 
 const App: React.FC = () => {
   return (
-    <Router history={history}>
+    <BrowserRouter>
       <GlobalStyle />
-      <Header />
       <SuspenseWithChunkError fallback={<PageLoader />}>
         <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
+          <CustomRoute path="/" exact component={Home} />
           <Route component={NotFound} />
         </Switch>
       </SuspenseWithChunkError>
-    </Router>
+    </BrowserRouter>
   )
 }
 
--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,28 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Button, Text, Heading } from 'components'
-import Page from 'components/layout/Page'
 
 const StyledNotFound = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
-  height: calc(100vh - 64px);
+  height: 100vh;
   justify-content: center;
 `
 
 const NotFound = () => {
 
   return (
-    <Page>
-      <StyledNotFound>
-        <Heading scale="xxl">404</Heading>
-        <Text mb="16px">Oops, page not found.</Text>
-        <Button as="a" href="/" scale="sm">
-          Back Home
-        </Button>
-      </StyledNotFound>
-    </Page>
+    <StyledNotFound>
+      <Heading scale="xxl">404</Heading>
+      <Text mb="16px">Oops, page not found.</Text>
+      <Button as="a" href="/" scale="sm">
+        Back Home
+      </Button>
+    </StyledNotFound>
   )
 }
 
