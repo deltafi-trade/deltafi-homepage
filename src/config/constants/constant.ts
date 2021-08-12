e96c021db7397e7a5750b03084759470192506aa
dennis-1210 <dennis1210@deltafi.ai>
2021-08-12 11:18:16 -0400

    update social link

--- src/config/constants/constant.ts
+++ src/config/constants/constant.ts
@@ -1,8 +1,8 @@
 export const HOMEPAGE_LINK = process.env.REACT_HOMPAGE_URL || '/'
 export const APP_LINK = process.env.REACT_APP_URL || 'http://localhost:3001'
-export const BLOG_LINK = 'https://blog.com'
-export const TWITTER_LINK = 'https://twitter.com'
-export const DISCORD_LINK = 'https://discord.org'
+export const BLOG_LINK = 'https://medium.com/deltafi'
+export const TWITTER_LINK = 'https://twitter.com/deltafi_ai'
+export const DISCORD_LINK = 'https://discord.com/invite/6maaM2cYqr'
 export const GITHUB_LINK = 'https://github.com'
 export const PINTEREST_LINK = "https://pinterest.com"
 export const YOUTUBE_LINK = "https://youtube.com"
