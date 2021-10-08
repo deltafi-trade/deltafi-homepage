13de9408ac4a3cdc66e2349843ede51616cfd04a
tchen017 <taylor@deltafi.ai>
2021-10-08 17:10:24 -0400

    Update images and clean unused assets

--- src/theme/types.ts
+++ src/theme/types.ts
@@ -1,92 +1,90 @@
-export type Breakpoints = string[];
+export type Breakpoints = string[]
 
 export type MediaQueries = {
-  xs: string;
-  sm: string;
-  md: string;
-  lg: string;
-  xl: string;
-  nav: string;
-};
+  xs: string
+  sm: string
+  md: string
+  lg: string
+  xl: string
+  nav: string
+}
 
-export type Spacing = number[];
+export type Spacing = number[]
 
 export type Radii = {
-  small: string;
-  default: string;
-  card: string;
-  circle: string;
-};
+  small: string
+  default: string
+  card: string
+  circle: string
+}
 
 export type Shadows = {
-  level1: string;
-  active: string;
-  success: string;
-  warning: string;
-  focus: string;
-  inset: string;
-};
+  level1: string
+  active: string
+  success: string
+  warning: string
+  focus: string
+  inset: string
+}
 
 export type Gradients = {
-  bubblegum: string;
-  inverseBubblegum: string;
-  cardHeader: string;
-  blue: string;
-  violet: string;
-  violetAlt: string;
-  gold: string;
-  asset: string;
-};
+  bubblegum: string
+  inverseBubblegum: string
+  cardHeader: string
+  blue: string
+  violet: string
+  violetAlt: string
+  gold: string
+  asset: string
+}
 
 export type Colors = {
-  primary: string;
-  primaryBright: string;
-  primaryDark: string;
-  secondary: string;
-  tertiary: string;
-  success: string;
-  failure: string;
-  warning: string;
-  cardBorder: string;
-  contrast: string;
-  dropdown: string;
-  dropdownDeep: string;
-  invertedContrast: string;
-  input: string;
-  inputSecondary: string;
-  background: string;
-  backgroundDisabled: string;
-  backgroundAlt: string;
-  text: string;
-  textDisabled: string;
-  textSubtle: string;
-  disabled: string;
-  footer: string;
-  footerMain: string;
-  footerText: string;
-  btnColor: string;
-  investBackground: string;
-  cardBackground: string;
-  cardBoxShadow: string;
-  boxShadow: string;
-  mainBackground: string;
-  tabletBackground: string;
-  mobileBackground: string;
-  menuBackground: string;
-  backgroundDelta: string;
-  panelBackground: string;
-  expandMenu: string;
-  subMenu: string;
+  primary: string
+  primaryBright: string
+  primaryDark: string
+  secondary: string
+  tertiary: string
+  success: string
+  failure: string
+  warning: string
+  cardBorder: string
+  contrast: string
+  dropdown: string
+  dropdownDeep: string
+  invertedContrast: string
+  input: string
+  inputSecondary: string
+  background: string
+  backgroundDisabled: string
+  backgroundAlt: string
+  text: string
+  textDisabled: string
+  textSubtle: string
+  disabled: string
+  footer: string
+  footerMain: string
+  footerText: string
+  btnColor: string
+  investBackground: string
+  cardBackground: string
+  cardBoxShadow: string
+  boxShadow: string
+  mainBackground: string
+  menuBackground: string
+  backgroundDelta: string
+  panelBackground: string
+  expandMenu: string
+  subMenu: string
 
   // Gradients
-  gradients: Gradients;
+  gradients: Gradients
 
   // Additional colors
-  binance: string;
-  overlay: string;
-};
+  binance: string
+  overlay: string
+}
 
 export type ZIndices = {
-  dropdown: number;
-  modal: number;
-};
+  dropdown: number
+  modal: number
+}
