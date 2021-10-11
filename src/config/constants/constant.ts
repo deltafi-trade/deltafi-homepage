cee7e10d4509b9b68bd4c75044b41f01f2443037
pjxthy <tojinxin@gmail.com>
2021-10-11 16:34:49 -0700

    Discord and telegram link update (#13)
    * update medium link
    
    * update telegram link

--- src/config/constants/constant.ts
+++ src/config/constants/constant.ts
@@ -1,10 +1,10 @@
 export const HOMEPAGE_LINK = process.env.REACT_HOMPAGE_URL || '/'
 export const APP_LINK = process.env.REACT_APP_URL || 'https://app.deltafi.ai'
-export const BLOG_LINK = 'https://deltafi.medium.com'
+export const BLOG_LINK = 'https://medium.com/deltafi'
 export const TWITTER_LINK = 'https://twitter.com/deltafi_ai'
 export const DISCORD_LINK = 'https://discord.gg/deltafi'
 export const GITHUB_LINK = 'https://github.com/delta-fi'
-export const TELEGRAM_LINK = 'https://t.me/deltafi_ai'
+export const TELEGRAM_LINK = 'https://t.me/deltafi_labs'
 export const YOUTUBE_LINK = "https://youtube.com"
 export const CAREERS_LINK = "https://deltafi.medium.com/jobs-deltafi-280dd54f82de"
 export const PRIVACY_LINK = "/privacy"
