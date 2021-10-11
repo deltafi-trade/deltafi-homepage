02073c90cc0ecdb591df6bba9aca33a56e77be80
Xin <tojinxin@gmail.com>
2021-10-11 16:02:33 -0700

    update medium link

--- src/config/constants/constant.ts
+++ src/config/constants/constant.ts
@@ -1,6 +1,6 @@
 export const HOMEPAGE_LINK = process.env.REACT_HOMPAGE_URL || '/'
 export const APP_LINK = process.env.REACT_APP_URL || 'https://app.deltafi.ai'
-export const BLOG_LINK = 'https://deltafi.medium.com'
+export const BLOG_LINK = 'https://medium.com/deltafi'
 export const TWITTER_LINK = 'https://twitter.com/deltafi_ai'
 export const DISCORD_LINK = 'https://discord.gg/deltafi'
 export const GITHUB_LINK = 'https://github.com/delta-fi'
