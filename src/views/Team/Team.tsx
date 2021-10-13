4730c7272e6897cbc487271b27253049af158d97
KaiJack0105 <kai@deltafi.ai>
2021-10-14 01:08:00 +0800

    fix: remove comment

--- src/views/Team/Team.tsx
+++ src/views/Team/Team.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Page from 'components/layout/Page'
 
-// import AssetManager from '../Home/components/AssetManager'
 import TeamMember from './components/TeamMember'
 
 const Home: React.FC = () => {
