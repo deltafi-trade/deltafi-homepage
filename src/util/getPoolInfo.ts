9855bd4823efb1a9c940fcd11b8bbb93b39682de
leqiang-deltafi <leqiang@deltafi.ai>
2022-05-27 19:48:19 -0700

    x

---
+++ src/util/getPoolInfo.ts
@@ -0,0 +1,15 @@
+
+// import fullDeploymentConfigV2 from "../config/constants/fullDeploymentConfigV2.json"
+// import { Connection, clusterApiUrl, Cluster } from "@solana/web3.js"
+
+const deploymentName = "mainnet-test";
+
+export function parseDeploymentConfig() {
+  // const deploymentConfig = fullDeploymentConfigV2[deploymentName];
+  // const connection = new Connection(clusterApiUrl(deploymentConfig.network as Cluster), "confirmed");
+  // const symbolToUrlMap = {};
+
+  // for (const tokenInfo in deploymentConfig.tokenInfoList) {
+  //   console.log(tokenInfo)
+  // }
+}
